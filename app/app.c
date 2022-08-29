3d45c30b8eec3e842b5da49c22f39cab40681b3b
Hector <you@example.com>
2022-08-28 22:09:05 -0500

    Modificaciones finales

--- app/app.c
+++ app/app.c
@@ -2,36 +2,36 @@
 #include "pointers.h"
 int main(void)
 {
-    int no_order_vector[4] = {55, 55,23,1,};
+    int no_order_vector[7] = {55, 55,23,1,5894,5,2};
     int *vector_ptr = &no_order_vector[0];
 
     //Metodo de burbuja
 
-    /*
+
      int changes = bubble_sort(vector_ptr,sizeof(no_order_vector)/sizeof(no_order_vector[0]));
     printf("%d intercambios para ordenar el arreglo con el metodo de burbuja.\n",changes);
     show_Array(no_order_vector,sizeof(no_order_vector)/sizeof(no_order_vector[0]) );
-    printf("\n");
-      */
+    printf("\n\n");
+
 
     //Metodo de inserción
 
-     /*
+
    insertion_sort(vector_ptr,sizeof(no_order_vector)/sizeof(no_order_vector[0]));
    printf("\n Observamos el cambio en el arreglo original\n");
-   show_Array(no_order_vector, 7);
-   printf("\n");
+   show_Array(no_order_vector, sizeof(no_order_vector)/sizeof(no_order_vector[0]));
+   printf("\n\n");
 
-    */
+
 
   //Método reverse
-    /*
+
 
    reverse(vector_ptr,sizeof(no_order_vector)/sizeof(no_order_vector[0]));
    printf("\n Observamos el cambio en el arreglo original\n");
    show_Array(no_order_vector, sizeof(no_order_vector)/sizeof(no_order_vector[0]));
-   printf("\n");
-  */
+   printf("\n\n");
+
 
      //Frequency
 
